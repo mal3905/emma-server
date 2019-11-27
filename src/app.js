6a31b0acf1191c5b3070c977ea24a7177d318aa5
Maria Danielson <mal3905@gmail.com>
2019-11-27 14:52:23 -0500

    final

--- src/app.js
+++ src/app.js
@@ -20,7 +20,13 @@ app.use(helmet())
 app.use(cors())
 app.use('/api/items', itemsRouter)
 app.use('/api/category', categoryRouter)
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 
 app.get('/', (req, res) => {
